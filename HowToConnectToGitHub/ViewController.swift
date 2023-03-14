aca0bead6d5d28d7672992df4dbf3661cc1ac991
eduardo salcedo <salcedo.eduardo@gmail.com>
2023-03-14 08:40:47 -0400

    Add PRAGMA mark for IBOutlets

--- HowToConnectToGitHub/ViewController.swift
+++ HowToConnectToGitHub/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
